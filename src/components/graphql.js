0ef6df23c8e8f9e2a8962f30da68ce69a06fecdc
hassanAdamiNejad <adaminejadh@gmail.com>
2017-08-12 14:00:58 +0430

    add graohql.js vs service_type.gql

--- src/components/graphql.js
+++ src/components/graphql.js
@@ -20,7 +20,8 @@ import { graphql } from 'react-apollo';
 // GraphQL queries.  Looking at this file demonstrates how to import fragments.
 // Webpack will compile this into inline GraphQL for us, so we can pass the
 // query to components using the @graphql decorator
-import allMessages from 'src/graphql/queries/all_messages.gql';
+// import allMessages from 'src/graphql/queries/all_messages.gql';
+import serviceType from 'src/graphql/queries/service_type.gql';
 
 // ----------------------
 
@@ -28,20 +29,22 @@ import allMessages from 'src/graphql/queries/all_messages.gql';
 // `react-apollo`'s `graphql` HOC/decorator and pass in the query that this
 // component requires.   Note: This is not to be confused with the `graphql`
 // lib, which is used on the server-side to initially define the schema
-@graphql(allMessages)
+@graphql(serviceType)
 export default class GraphQLMessage extends React.PureComponent {
   static propTypes = {
     data: PropTypes.shape({
-      message: PropTypes.shape({
-        text: PropTypes.string,
+      serviceType: PropTypes.shape({
+        name: PropTypes.string,
+        engName: PropTypes.string,
       }),
     }),
   }
 
   static defaultProps = {
     data: {
-      message: {
-        text: null,
+      serviceType: {
+        name: null,
+        engName: null,
       },
     },
   }
@@ -52,15 +55,16 @@ export default class GraphQLMessage extends React.PureComponent {
     // Since we're dealing with async GraphQL data, we defend against the
     // data not yet being loaded by checking to see that we have the `message`
     // key on our returned object
-    const message = data.message && data.message.text;
+    const serviceTypeName = data.serviceType && data.serviceType.name;
 
     // Apollo will tell us whether we're still loading.  We can also use this
     // check to ensure we have a fully returned response
-    const isLoading = data.loading ? 'yes' : 'nope';
+    if (data.loading) {
+      return null;
+    }
     return (
       <div>
-        <h2>Message from GraphQL server: <em>{message}</em></h2>
-        <h2>Currently loading?: {isLoading}</h2>
+        <h2>Message from GraphQL server: <em>{serviceTypeName}</em></h2>
       </div>
     );
   }
